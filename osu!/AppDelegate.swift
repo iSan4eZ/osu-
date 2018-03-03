d5bff317e6f1bc7a0469caa195a8eb1cee5ffbf0
Stas <staspanasuk@mail.ru>
2018-03-03 22:25:17 +0200

    Added automatic unpacking of .osz files that are in Documents. Added support to unpack external .osz by "Open in..."

--- osu!/AppDelegate.swift
+++ osu!/AppDelegate.swift
@@ -13,15 +13,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    let main = MainMenu()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        print(url)
-        return true
+        print("External URL to unzip: \(url)")
+        if url.startAccessingSecurityScopedResource(){
+            print("File exists in App Delegate: \(FileManager.default.fileExists(atPath: url.path))")
+            main.unZip(url: url, deleteAfter: false)
+            return true
+        } else {
+            return false
+        }
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- osu!/MainMenu.swift
+++ osu!/MainMenu.swift
@@ -19,6 +19,7 @@ class MainMenu: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Zip.addCustomFileExtension("osz")
         
         initialConstraints.append(self.mainButton.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 1/2))
         initialConstraints.append(self.mainButton.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 1/2))
@@ -88,12 +89,43 @@ class MainMenu: UIViewController {
          -Каждый url в общедоступном каталоге приложения (который из Files открывается) проверять на .zip. Если .zip, то вызывать unZip().
          -В локальном хранилище создавать, если отсутствует, папку Library, в которой будут все карты, а так же Skins для скинов.
         */
+        let fileManager = FileManager.default
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        do {
+            let fileURLs = try fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil)
+            for url in fileURLs{
+                print("\nExtension: \(url.pathExtension)")
+                if url.pathExtension == "osz"{
+                    unZip(url: url, deleteAfter: true)
+                }
+            }
+        } catch {
+            print("Error while enumerating files")
+        }
     }
     
-    func unZip(url: URL){
+    func unZip(url: URL, deleteAfter: Bool){
         /*
          -По отправленному URL разархивировать содержимое в папку Library. После этого удалять архив
          */
+        do
+        {
+            let manager = FileManager.default
+            let documentsDirectory = manager.urls(for:.documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Library/\(url.deletingPathExtension().lastPathComponent)", isDirectory: true)
+            try Zip.unzipFile(url, destination: documentsDirectory, overwrite: true, password: nil)
+            if deleteAfter{
+                try manager.removeItem(at: url)
+            }
+            url.stopAccessingSecurityScopedResource()
+            /*let unzipDirectory = try Zip.quickUnzipFile(url)
+            print("Unziped Directory: \(unzipDirectory)")
+            let path = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Library/\(url.deletingPathExtension().lastPathComponent)", isDirectory: true)
+            print("Unzip path: \(path)")
+            try fileManager.createDirectory(at: path, withIntermediateDirectories: true, attributes: nil)
+            try fileManager.moveItem(at: unzipDirectory, to: path)*/
+        } catch {
+            print("Error while unzipping: \(error)\n")
+        }
     }
     
     override func didReceiveMemoryWarning() {
